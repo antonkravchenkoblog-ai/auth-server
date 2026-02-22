15f2490a43434cbee4160fe9a1eefca32bffed0b
Anton <antonkravchenkoblog@gmail.com>
2026-02-22 14:15:12 +0100

    fix: session setup

--- src/main.ts
+++ src/main.ts
@@ -31,11 +31,11 @@ async function bootstrap() {
       resave: false,
       saveUninitialized: false,
       cookie: {
-        domain: config.getOrThrow<string>('SESSION_DOMAIN'),
+        domain: config.get<string>('SESSION_DOMAIN') || undefined,
         maxAge: ms(config.getOrThrow<StringValue>('SESSION_MAX_AGE')),
         httpOnly: parseBoolean(config.getOrThrow<string>('SESSION_HTTP_ONLY')),
         secure: parseBoolean(config.getOrThrow<string>('SESSION_SECURE')),
-        sameSite: 'lax',
+        sameSite: (config.get<string>('SESSION_SAME_SITE') || 'lax') as 'lax' | 'strict' | 'none',
       },
     }),
   );
