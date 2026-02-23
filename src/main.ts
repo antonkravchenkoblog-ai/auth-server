a90cae319f451ff938341515f95985f00a114bb3
Anton <antonkravchenkoblog@gmail.com>
2026-02-24 00:40:34 +0100

    fix: set up redis

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,18 @@
-import { ValidationPipe } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
-import { NestFactory } from '@nestjs/core';
-import * as cookieParser from 'cookie-parser';
+import { ValidationPipe } from '@nestjs/common'
+import { ConfigService } from '@nestjs/config'
+import { NestFactory } from '@nestjs/core'
+import * as cookieParser from 'cookie-parser'
+import * as session from 'express-session'
+import IORedis from 'ioredis'
 
-import * as session from 'express-session';
+import { ms, StringValue } from '@/libs/utils/ms.util'
+import { parseBoolean } from '@/libs/utils/parse-boolean.util'
+import { RedisStore } from 'connect-redis'
+import { AppModule } from './app.module'
 
-import { AppModule } from './app.module';
-import { ms, StringValue } from '@/libs/utils/ms.util';
-import { parseBoolean } from '@/libs/utils/parse-boolean.util';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
   const config = app.get(ConfigService);
 
   app.use(cookieParser(config.getOrThrow<string>('COOKIES_SECRET')));
@@ -23,9 +24,16 @@ async function bootstrap() {
       forbidNonWhitelisted: true,
     }),
   );
+  
+  const redis = new IORedis(config.getOrThrow<string>('REDIS_URL'));
+
 
   app.use(
     session({
+      store: new RedisStore({
+        client: redis,
+        prefix: config.get<string>('REDIS_SESSION_PREFIX'),
+      }),
       secret: config.getOrThrow<string>('SESSION_SECRET'),
       name: config.getOrThrow<string>('SESSION_NAME'),
       resave: false,
@@ -35,8 +43,11 @@ async function bootstrap() {
         maxAge: ms(config.getOrThrow<StringValue>('SESSION_MAX_AGE')),
         httpOnly: parseBoolean(config.getOrThrow<string>('SESSION_HTTP_ONLY')),
         secure: parseBoolean(config.getOrThrow<string>('SESSION_SECURE')),
-        sameSite: (config.get<string>('SESSION_SAME_SITE') || 'lax') as 'lax' | 'strict' | 'none',
-      },
+        sameSite: (config.get<string>('SESSION_SAME_SITE') || 'lax') as
+          | 'lax'
+          | 'strict'
+          | 'none',
+      }
     }),
   );
 
